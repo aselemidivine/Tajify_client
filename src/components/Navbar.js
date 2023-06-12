a71586cee781c5118e0b7f19571778c72185d162
Aselemi Divine <aselemidivine@gmail.com>
2023-06-13 00:34:11 +0100

    navbar works

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+        <div className="nav-wrapper white">
+          <a href="#" className="brand-logo left">Logo</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
