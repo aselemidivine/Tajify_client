f4ab03b184d3f579582fc577ae315a43ceb65351
idowudamolaolatunji <olatunjiidowuoyin06@gmail.com>
2023-08-08 16:43:41 +0100

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,38 @@
-import HeroImgs from '../assets/images/hero-img/photo1.png';
+import HeroImg from '../assets/images/hero-img/photo1.png';
+import Shield from '../assets/images/hero-img/sheild.svg';
+import { AiOutlineSearch } from "react-icons/ai";
+import '../pages/blogHome/style.css';
 
-import '../pages/blogHome/style.css'
 
 function Hero() {
     return (
         <div className="hero__section">
-            <div className="container">
+            <div className="section__container">
                 <div className="hero">
                     <div className="hero__text--box">
                         <h1 className="heading__primary">
-                            Monitize your 
+                            Monitize your&nbsp;
                             <span className="hero--extra">creativity</span> and trive in a digital world.
                         </h1>
                         <h4 className="heading__description">
-                        The ultimate platform created with one purpose in mind: to help creators like you monetize your <span className="hero--extra">content, skills, knowledge, passion, followership, and writing abilities.</span>
+                        The ultimate platform created with one purpose in mind: to help creators like you monetize your&nbsp;<span className="hero--extra">content, skills, knowledge, passion, followership, and writing abilities.</span>
                         </h4>
                         <form className="hero__form">
-                            <input className='hero__input' />
+                            <i className='hero__form--icon'>
+                                <AiOutlineSearch />
+                            </i>
+                            <input className='hero__form--input' />
                             <button className='hero__form--btn' type='button'>Search</button>
                         </form>
 
                         <div className="hero__stats">
-                            {/* svg goes here */}
+                            {/* <Shield /> */}
                             <Stat figure='1M+' text='services' />
                             <Stat figure='1200+' text='Active users' />
                         </div>
                     </div>
                     <div className="hero__images--box">
-                        <img src={HeroImgs} alt="hero photo tajify" className="hero__image" />
+                        <img src={HeroImg} alt="hero photo tajify" className="hero__image" />
                     </div>
                 </div>
             </div>
