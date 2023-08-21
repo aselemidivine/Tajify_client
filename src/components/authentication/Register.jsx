267640ccb665655c2c439849e87eb5f8dbe43743
idowudamolaolatunji <olatunjiidowuoyin06@gmail.com>
2023-08-21 21:53:36 +0100

    finished signup

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -3,8 +3,12 @@ import React, { useState } from 'react';
 import '../../index.css';
 import './auth.css';
 
+import { HiOutlineKey } from 'react-icons/hi';
+import { BiSolidUser } from 'react-icons/bi';
+import { FaRegEyeSlash } from 'react-icons/fa';
+import { HiOutlinePhone } from 'react-icons/hi';
 import { FcGoogle } from 'react-icons/fc';
-import Image1 from '../../assets/images/pngs/login-slide-img-1.png';
+import SignupImg from '../../assets/images/pngs/signup-img.png';
 
 
 // import { Carousel } from 'react-slider';
@@ -48,46 +52,52 @@ import Image1 from '../../assets/images/pngs/login-slide-img-1.png';
 
 const Register = () => {
   return (
-    <div className="signup__section">
-      <div className="signup__container signup">
-        <div className="auth">
-          <h3 className="auth__heading">Signup</h3>
-          <div className="auth__head">
-            <div className="auth__head--card">
-                <FcGoogle />
-                <p>Connect with Google</p>
+    <section className="signup__section">
+      <div className="signup__container ">
+        <div className="signup">
+          <div>
+            <h3 className="auth__heading">Signup</h3>
+            <div className="auth__head">
+              <div className="auth__head--card">
+                  <FcGoogle />
+                  <p>Connect with Google</p>
+              </div>
             </div>
-          </div>
 
-          <span className='auth__mid'>
-              <p> or register with email </p>
-          </span>
+            <span className='auth__mid'>
+                <p> or register with email </p>
+            </span>
 
-          <form className="login__form" autoComplete='off'>
-            <div className="form__item">
-                <input type="text" className="form__input" placeholder='username'/>
-            </div>
-            <div className="form__item">
+            <form className="login__form" autoComplete='off'>
+              <div className="form__item">
+                  <BiSolidUser className='input__icon' />
+                  <input type="text" className="form__input" placeholder='username'/>
+              </div>
+              <div className="form__item">
+                <HiOutlinePhone className='input__icon' />
                 <input type="tel" className="form__input" placeholder='phone'/>
-            </div>
-            <div className="form__item">
-                <input type="password" className="form__input" placeholder='password'/>
-            </div>
-            <div className="form__item">
-                <input type="password" className="form__check-box" id='check'/>
-                <label htmlFor="check" className="form__label">I agree to the <a className='form__link'>Terms </a> & <a className='form__link'>Privacy policy</a></label>
-            </div>
-              
-            <div className="form__item">
-                <button className="form__submit">Create Account</button>
-            </div>
-          </form>
-        </div>
-        <div className="signup__images--box">
-            <img src={Image1} alt="signup image" />
+              </div>
+              <div className="form__item">
+                  <HiOutlineKey className='input__icon' />
+                  <input type="password" className="form__input" placeholder='Password'/>
+                  <FaRegEyeSlash className='password__icon' />
+              </div>
+              <div className="form__flex">
+                <input type="checkbox" className="form__check-box" id='check'/>
+                <label htmlFor="checkbox" className="form__label">I agree to the <a href="#" className='form__link'>Terms </a> & <a href="#" className='form__link'>Privacy policy</a>.</label>
+              </div>
+                
+              <div className="form__item">
+                  <button className="form__submit">Create Account</button>
+              </div>
+            </form>
+          </div>
+          <div className="signup__images--box">
+            <img src={SignupImg} alt="signup image" />
+          </div>
         </div>
       </div>
-    </div>      
+    </section>      
 );
       
 
