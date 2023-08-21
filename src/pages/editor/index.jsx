836c0927679904886879e6916cb812a56e069bd5
Aselemi Divine <aselemidivine@gmail.com>
2023-08-21 17:58:04 +0100

    styling editors page

--- src/pages/editor/index.jsx
+++ src/pages/editor/index.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import Header from '../../components/Header'
+import React, { useState } from "react";
+import Header from "../../components/Header";
 import "../../pages/blogDetails/blogDetails.css";
-
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import parse from 'html-react-parser'
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const Editor = () => {
-  return (
-      <div className="blog__container">
-        <Header />
-      {/* EDITOR */}
-    </div>
-  )
-}
+  const [text, setText] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [subHeading, setSubHeading] = useState("");
 
-export default Editor
+  return (
+    <div className="blog__container">
+      <Header />
+      <div className="custom__width editor__width">
+
+      <div className="editor">
+        <CKEditor
+          editor={ClassicEditor}
+          data={text}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setText(data)
+          }}
+        />
+      </div>
+      {/* <div className="editor__content">
+        <h1>Title</h1>
+        <h4>Header</h4>
+        <p>{parse(text)}</p>
+
+      </div> */}
+    </div>
+    </div>
+  );
+};
+
+export default Editor;
