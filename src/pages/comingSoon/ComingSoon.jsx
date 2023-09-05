82c842c70a9c086efecf42ee474938b0aea28513
idowudamolaolatunji <olatunjiidowuoyin06@gmail.com>
2023-09-05 14:24:19 +0100

    coming soon

--- src/pages/comingSoon/ComingSoon.jsx
+++ src/pages/comingSoon/ComingSoon.jsx
@@ -4,18 +4,22 @@ import { useNavigate } from 'react-router-dom'
 const ComingSoon = () => {
     const navigate = useNavigate();
   return (
-    <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-    <h1 class="text-5xl text-white font-bold mb-8 animate-pulse">
-        Coming Soon
-    </h1>
-    <p class="text-white text-center text-lg mb-8">
-        We're working hard to bring you something amazing. Stay tuned!
-    </p>
+    <div class="" style={{backgroundColor: '#fff', display: "flex", justifyContent: "center", alignItems: "center", width: '100%', height: '100vh'}}>
+      {/* min-h-screen bg-gray-900 flex flex-col items-center justify-center */}
+      <span style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
 
-    <div className='flex justify-center'>
-        <button className='btn-primary' onClick={() => navigate(-1)}>Go back</button>
-    </div>
-</div>
+        <h1 class="text-5xl font-bold mb-8 animate-pulse" style={{color: '#008001'}}>
+            Coming Soon
+        </h1>
+        <p class="text-grey-900 text-center text-xlg mb-8">
+            We're working hard to bring you something amazing. Stay tuned!
+        </p>
+
+        <div className='flex justify-center'>
+            <button className='btn-primary' style={{color: '#444'}} onClick={() => navigate(-1)}>Go back</button>
+        </div>
+      </span>
+  </div>
   )
 }
 
